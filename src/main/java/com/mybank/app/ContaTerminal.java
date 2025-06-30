a3361c8579e6a807121725c0f1346a0172ae4ce1
Lucas Rosas <lucasroscun@gmail.com>
2025-06-29 22:58:05 -0300

    Project setup

---
+++ src/main/java/com/mybank/app/ContaTerminal.java
@@ -0,0 +1,10 @@
+package com.mybank.app;
+
+/**
+ * Hello world!
+ */
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World!");
+    }
+}
